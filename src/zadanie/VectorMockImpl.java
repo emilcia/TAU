0f6932aed1183cb9d41746da224d25567ed408fa
s12594 <s12594@lab1l-15.pjwstk.edu.pl>
2017-02-28 11:20:35 +0100

    zadanie1 - pierwszy commit

---
+++ src/zadanie/VectorMockImpl.java
@@ -0,0 +1,34 @@
+package zadanie;
+
+import java.util.List;
+
+public class VectorMockImpl implements Vector {
+
+	@Override
+	public Vector add(Vector v) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Vector addVectors(Vector a, Vector b) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void set(List<Double> list) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List get(List<Double> list) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
+
+
